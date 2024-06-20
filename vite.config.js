972274aeaf296f350320507a6ec408a10539c3f5
Himanshu <himanshuingle42@gmail.com>
2024-06-20 19:41:41 +0530

    Code Update!

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RefokusClone/' // Replace 'RefokusClone' with the actual repository name
+  base: "/RefokusClone/", // Replace 'RefokusClone' with the actual repository name
+  build: {
+    assetsDir: "assets", // Directory where Vite will output static assets
+    sourcemap: true, // Enable sourcemaps for debugging
+  },
 });
